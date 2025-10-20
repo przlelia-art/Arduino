aa8cca42535f6414458dc57a20533d0611e9c719
lelia <perez_l@etna-alternance.net>
2025-10-20 15:43:13 +0200

    allumage des 2 leds en même temps

---
+++ etape1_scenario1/etape1_scenario1.ino
@@ -0,0 +1,8 @@
+// Programme de test pour faire clignoter la LED intégrée toutes les secondes
+    void setup() {
+     pinMode(13, OUTPUT); // Définir la LED intégrée en mode sortie
+    }
+
+    void loop() {
+     digitalWrite(13, HIGH); // Allume la LED
+    }
