72acd83a6c3e0b5d7aad04b12c18639016348467
lelia <perez_l@etna-alternance.net>
2025-10-20 16:00:49 +0200

    ajustement du code

--- etape1_scenario1/etape1_scenario1.ino
+++ etape1_scenario1/etape1_scenario1.ino
@@ -1,8 +1,10 @@
 // Programme de test pour faire clignoter la LED intégrée toutes les secondes
     void setup() {
      pinMode(13, OUTPUT); // Définir la LED intégrée en mode sortie
+     pinMode(12, OUTPUT);
     }
 
     void loop() {
      digitalWrite(13, HIGH); // Allume la LED
+     digitalWrite(12, HIGH); // Allume la LED
     }
