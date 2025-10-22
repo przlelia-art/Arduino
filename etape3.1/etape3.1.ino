b8b2f6bfd48e9b6b50e271f926beaae9e5b30a27
lelia <perez_l@etna-alternance.net>
2025-10-22 09:33:24 +0200

    ajustement du script par rapport aux températures

--- etape3.1/etape3.1.ino
+++ etape3.1/etape3.1.ino
@@ -14,7 +14,7 @@ LiquidCrystal_I2C lcd(0x27,  16, 2);
 #define DHTTYPE DHT11
 DHT dht(DHTPIN, DHTTYPE);
 
-int Temperature = analogRead(A0);
+int Temperature = dht.readTemperature();
 
 void setup() {
    pinMode(13, OUTPUT); // Définir la LED intégrée en mode sortie
@@ -47,19 +47,25 @@ void loop() {
   // you can change whats in the quotes to be what you want  it to be!
   lcd.print("Humidite = " + String(dht.readHumidity())+" %");
 
-   if(Temperature < 18);
-   digitalWrite(13, HIGH);
-   digitalWrite(12, LOW);
-   digitalWrite(11, LOW);
 
-   
-   if(Temperature >= 18 && Temperature <= 22);
-   digitalWrite(13, LOW);
-   digitalWrite(12, HIGH);
-   digitalWrite(11, LOW);
-
-   if(Temperature < 22);
+if(Temperature < 22){
    digitalWrite(13, LOW);
    digitalWrite(12, LOW);
    digitalWrite(11, HIGH);
+   }
+   
+   else if(Temperature >= 18 && Temperature <= 22){
+   digitalWrite(13, LOW);
+   digitalWrite(12, HIGH);
+   digitalWrite(11, LOW);
+   }
+
+   else if(Temperature < 18){
+   digitalWrite(13, HIGH);
+   digitalWrite(12, LOW);
+   digitalWrite(11, LOW);
+   }
 }
+
+
+  
