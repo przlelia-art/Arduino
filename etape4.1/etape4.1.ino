9ee11dd9944b6e18db9743674654e36357ba242d
lelia <perez_l@etna-alternance.net>
2025-10-22 11:38:40 +0200

    fix: correction du code pour pouvoir afficher les degrés et les pourcentage sur l'écran I2C

--- etape4.1/etape4.1.ino
+++ etape4.1/etape4.1.ino
@@ -20,9 +20,7 @@ void setup() {
    pinMode(13, OUTPUT);
    pinMode(12, OUTPUT);
    pinMode(11, OUTPUT);
-   pinMode(10, OUTPUT);
-   pinMode(9, OUTPUT);
-   pinMode(8, OUTPUT);
+  
 
   
   //initialize lcd screen
@@ -43,12 +41,12 @@ void loop() {
   // tell the screen to write on the top row
   lcd.setCursor(0,0);
   // tell the screen to write “hello, from” on the top  row
-  lcd.print("Temperature = " + String(dht.readTemperature())+" °C");
+  lcd.print("Temp = " + String(dht.readTemperature())+" C");
   // tell the screen to write on the bottom  row
   lcd.setCursor(0,1);
   // tell the screen to write “Arduino_uno_guy”  on the bottom row
   // you can change whats in the quotes to be what you want  it to be!
-  lcd.print("Humidite = " + String(dht.readHumidity())+" %");
+  lcd.print("Humi = " + String(dht.readHumidity())+" %");
 
    if(Temperature < 18){
    digitalWrite(13, HIGH);
