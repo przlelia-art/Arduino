709a1dba46db849319388456cc3d27689a2aa02f
lelia <perez_l@etna-alternance.net>
2025-10-23 15:00:46 +0200

    amélioration de l'affichage sur l'écran I2C

--- etape4.2/etape4.2.ino
+++ etape4.2/etape4.2.ino
@@ -1,78 +1,55 @@
-
 #include <LiquidCrystal_I2C.h>
-
-#include  <Wire.h>
-
-LiquidCrystal_I2C lcd(0x27,  16, 2);
-
+#include <Wire.h>
 #include "DHT.h"
+
 #define DHTPIN 2
 #define DHTTYPE DHT11
 DHT dht(DHTPIN, DHTTYPE);
+LiquidCrystal_I2C lcd(0x27,  16, 2);
 
 void setup() {
-   pinMode(13, OUTPUT);
-   pinMode(12, OUTPUT);
-   pinMode(11, OUTPUT);
-
+  Serial.begin(9600);
   lcd.init();
   lcd.backlight();
-  Serial.begin(9600);
   dht.begin();
-
-  int Temp = dht.readTemperature();
-  int Humi = dht.readHumidity();
-  
 }
-
 void loop() {
-  
-  delay(1000);
+  int Temperature = dht.readTemperature();
+  int Humidite = dht.readHumidity();
+
+  delay(5000);
+
+  lcd.clear();
   lcd.setCursor(0,0);
-  lcd.print("Temp = " + String(dht.readTemperature())+" °C");
+  lcd.print("Temperature= " + String(Temperature) + "C");
+
   lcd.setCursor(0,1);
-  lcd.print("Humi = " + String(dht.readHumidity())+" %");
+  if(Temperature <18){
+    lcd.print("Temp basse");
+  }
+  else if(Temperature >=18 && Temperature<=22){
+    lcd.print("Temp ideal");
+  }
+  else{
+    lcd.print("Temp elevee");
+  }
 
-   if(Temp < 18){
-   digitalWrite(13, HIGH);
-   digitalWrite(12, LOW);
-   digitalWrite(11, LOW);
-   }
-   
-   else if(Temp >= 18 && Temp <= 22){
-   digitalWrite(13, HIGH);
-   digitalWrite(12, HIGH);
-   digitalWrite(11, LOW);
-   }
-
-   else (Temp > 22);{
-   digitalWrite(13, HIGH);
-   digitalWrite(12, HIGH);
-   digitalWrite(11, HIGH);
-   }
-   
-
-   if(Humi > 60){ 
-   digitalWrite(13, HIGH);
-   digitalWrite(12, LOW);
-   digitalWrite(11, LOW);
-   }
-   
-   else if(Humi >= 30 && Humi <= 60){
-   digitalWrite(13, LOW);
-   digitalWrite(12, HIGH);
-   digitalWrite(11, LOW);
-   }
-
-   else (Humi < 30);{
-   digitalWrite(13, LOW);
-   digitalWrite(12, LOW);
-   digitalWrite(11, HIGH);
-   }
-
-
-
-}
-
-
+  delay(5000);
   
+  lcd.clear();
+  lcd.setCursor(0,0);
+  lcd.print("Humidite = " + String(Humidite) + "%");
+
+  lcd.setCursor(0,1);
+  if(Humidite <30){
+    lcd.setCursor(0, 1);
+    lcd.print("Humi basse");
+  }
+  else if(Humidite >=30 && Humidite<=60){
+    lcd.print("Humi ideal");
+  }
+  else{
+    lcd.print("Humi elevee");
+
+  }
+}
